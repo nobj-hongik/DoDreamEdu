7c6af1b06ae656fabb010608be6e28311e7b9f94
nobj-hongik <shgustjd6262@naver.com>
2018-08-06 14:32:16 +0900

    1교시까지 내용

--- 4주차/lotto_example/main.js
+++ 4주차/lotto_example/main.js
@@ -4,30 +4,30 @@ user_lotto_numbers_list = []
 
 // 1 ~ 45 사이의 랜덤한 숫자 반환
 function getRandomNum(){
-  
+  return 5
 }
 
 // lotto_nums(list) 값들 중 ball_num(number)값과 중복되는 값이 있는지 확인한다.
 // 중복되는 값이 있으면 true 그렇지 않으면 false 반환
 function checkDuplicatedNum(lotto_nums, ball_num){
-    
+  return true
 }
 
 // getRandomNum 함수를 통해 lotto_nums(list)에 숫자 6개를 넣어 반환한다.
 // checkDuplicatedNum 함수를 통해 lotto 숫자에 중복된 숫자가 들어가지 않도록 한다.
 function getLottoNums(){
-  
+
 }
 
 // lotto_nums(list)에서 중복된 값이 있으면 true 그렇지 않으면 false 반환
 function checkDuplicatedNumInList(lotto_nums){
-   
+
 }
 
 // getLottoNums 함수를 통해 winning_numbers에 당첨번호를 위한 숫자 6개를 얻는다.
 // checkDuplicatedNum 함수를 통해 winning_numbers와 중복되지 않는 bonus_number를 얻는다.
 function getWinningNumbersAndBonusNumber(){
- 
+
 }
 
 // 파라미터 a와 b를 더한 값을 리턴한다.
@@ -37,7 +37,7 @@ function add(a, b){
 
 // 당첨번호 winning_number와 자신의 lotto_nums 중 일치하는 번호의 개수를 반환한다.
 function getMachedNum(lotto_nums){
-    
+
 }
 
 // getMachedNum 함수를 통해 얻은 값을 통해 '꽝', '5등', '4등', '3등', '2등', '1등'을 반환한다.
@@ -48,7 +48,7 @@ function getMachedNum(lotto_nums){
 // 2등 -> 5개 동일, 보너스 동일
 // 1등 -> 6개 동일
 function getWinningResult(lotto_nums){
-   
+
 }
 
 function addLottoNum(){
--- 4주차/lotto_example/main.spec.js
+++ 4주차/lotto_example/main.spec.js
@@ -1,1 +1,27 @@
 // TODO :: Write the testing code
+//로또 함수의 꾸러미를 만든 것
+// it()은 함수의 기능을 테스트
+// expect(x) : x에 대하여 기대한다는 의미
+// toBeGreaterThan0rEqual(1) : 이상인지
+
+describe("로또 함수의", function() {
+
+  describe("getRandomNum() 함수는", function() {
+    it("1~45 사이의 랜덤한 수를 생성한다.", function() {      // it()은 함수의 기능을 테스트
+      let rand_num = getRandomNum()
+      expect(parseInt(rand_num))
+      expect(rand_num).toBeGreaterThanOrEqual(1)
+      expect(rand_num).toBeLessThanOrEqual(45)
+    })
+  })
+
+  describe("checkDuplicatedNum() 함수는", function() {
+    it("[1,2,3,4,5,6], 5에 대해 중복 숫자가 있다.", function() {
+      expect(checkDuplicatedNum([1,2,3,4,5,6],5)).toBeTruthy()
+    })
+    it("[1,2,3,4,5,6], 10에 대해 중복 숫자가 없다.", function() {
+      expect(checkDuplicatedNum([1,2,3,4,5,6],10)).toBeFalsy()
+    })
+  })
+
+})
